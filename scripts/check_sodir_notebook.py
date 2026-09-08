0a66a0a0ec7ab58e50f055de8c6fe1a90f18373f
kkollsga <kkollsg@gmail.com>
2026-09-08 21:24:01 +0200

    Pin the corrected SODIR volume rules

--- scripts/check_sodir_notebook.py
+++ scripts/check_sodir_notebook.py
@@ -15,7 +15,7 @@ import sys
 ROOT = Path(__file__).resolve().parents[1]
 NOTEBOOK = ROOT / "docs" / "_static" / "notebooks" / "sodir-geologist.ipynb"
 RUNTIME_REVISION = "61e0534a89057535ba5a638dc9a83e2e0281cd78"
-DATASETS_REVISION = "bf6043c058e82f8715834812a4e63977f56b4b03"
+DATASETS_REVISION = "4db33e083bdbf78e729f3426064be27864e8bb2b"
 PIN_PATTERNS = {
     "kglite==0.17.1": r"(?<![\w-])kglite==0[.]17[.]1(?![\w.])",
     DATASETS_REVISION: re.escape(DATASETS_REVISION),
